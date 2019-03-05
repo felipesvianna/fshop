dd9a8fa2f9e1dc1129f2d89e26faccedc9fcbf07
Felipe <felipe.vianna10@gmail.com>
2019-03-05 17:14:12 -0300

    feat: Create tapableText widget

--- lib/widgets/tapableText.dart
+++ lib/widgets/tapableText.dart
@@ -1,6 +1,10 @@
 
 import 'package:flutter/material.dart';
 
+/*
+Widget that get an Text widget and a function.
+It applies the onTap function on the given text.
+*/
 Widget tapableText(Widget inputText, Function onTap) {
   return GestureDetector(
     onTap: onTap,
