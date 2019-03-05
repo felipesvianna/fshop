bae2307edc3dc0f68791f4b1c7783738480be789
Felipe <felipe.vianna10@gmail.com>
2019-03-05 18:24:54 -0300

    feat: Wrap the Column widget with SingleChildScrollView to scroll on login page

--- lib/screens/login_screen/login_page.dart
+++ lib/screens/login_screen/login_page.dart
@@ -22,16 +22,18 @@ class _LoginPageState extends State<LoginPage>{
             image: AssetImage("assets/img/bg_login_screen.png"), //Image bg
             fit:BoxFit.cover,
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image(
-                image: AssetImage("assets/img/logo.png")
-              ),
-              generateLoginForm(), //It returns a form
-              socialMediaButtons(), //It returns a row
-              generateSignUpLink(), //It returns a container
-            ],
+          SingleChildScrollView( //So the column can scroll
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                  image: AssetImage("assets/img/logo.png")
+                ),
+                generateLoginForm(), //It returns a form
+                socialMediaButtons(), //It returns a row
+                generateSignUpLink(), //It returns a container
+              ],
+            ),
           ),
         ],
       )
