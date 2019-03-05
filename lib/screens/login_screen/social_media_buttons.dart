5b5e60857110f911ebdaac2b554a0669887c2875
Felipe <felipe.vianna10@gmail.com>
2019-03-05 17:38:44 -0300

    feat: Create social media button widget

---
+++ lib/screens/login_screen/social_media_buttons.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+Widget socialMediaButtons(){
+  return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            RaisedButton(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text("Facebook"),
+              onPressed: () {},
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+            ),
+            RaisedButton(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              textColor: Colors.white,
+              color: Colors.red,
+              child: Text("Google"),
+              onPressed: () {},
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+            )
+          ]
+        );
+}
