c4ae36e9be2875b8812240a85a1191d80fad0f27
Felipe <felipe.vianna10@gmail.com>
2019-03-05 18:28:32 -0300

    refactor: Improve identation

--- lib/screens/login_screen/generate_signup_link.dart
+++ lib/screens/login_screen/generate_signup_link.dart
@@ -4,16 +4,16 @@ import '../../widgets/tapableText.dart';
 
 Widget generateSignUpLink(){
   return Container(
-                padding: EdgeInsets.only(top: 20, bottom: 14),
-                alignment: Alignment.center,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text('Novo por aqui? ', style: TextStyle(fontSize: 14),),
-                      tapableText(Text('Cadastre-se!', style: TextStyle(fontSize: 14, color: Colors.red, decoration: TextDecoration.underline)),
-                      (){print('Cadastre-se tocado');}
-                    )
-                  ]
-                )
-              );
+          padding: EdgeInsets.only(top: 20, bottom: 14),
+          alignment: Alignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Novo por aqui? ', style: TextStyle(fontSize: 14),),
+                tapableText(Text('Cadastre-se!', style: TextStyle(fontSize: 14, color: Colors.red, decoration: TextDecoration.underline)),
+                (){print('Cadastre-se tocado');}
+              )
+            ]
+          )
+        );
 }
