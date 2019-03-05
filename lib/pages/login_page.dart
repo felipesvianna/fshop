780ebea32b5cc23a31d4bfef422059b453e3768d
Felipe <felipe.vianna10@gmail.com>
2019-03-05 00:43:06 -0300

    feat: Create form on the login screen

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -17,6 +17,36 @@ class _LoginPageState extends State<LoginPage>{
           Image(
             image: AssetImage("assets/img/bg_login_screen.png"), //Image bg
             fit:BoxFit.cover,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage("assets/img/logo.png")
+              ),
+              Form(
+                child: Container(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "E-Mail"
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Senha"
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
           )
         ],
       ),
