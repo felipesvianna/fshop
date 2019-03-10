89eb29cb8c1b0cef3ca4ea6a9a1ae378a659e6b4
Felipe <felipe.vianna10@gmail.com>
2019-03-10 19:45:01 -0300

    test: Implement test_if_onboarding_screen_loads

--- test/widget_on_boarding_screen_test.dart
+++ test/widget_on_boarding_screen_test.dart
@@ -7,7 +7,16 @@ import './testing_util.dart';
 import 'package:fshop/screens/onboarding_screen/onboarding_screen.dart';
 
 void main() {
-  //test_if_onboarding_screen_loads
+  testWidgets('test_if_onboarding_screen_loads', (WidgetTester tester) async {
+    await tester.pumpWidget(buildTestableWidget(OnBoardingPage()));
+    await tester.pumpAndSettle();
+
+    var findByText = find.byType(Text);
+    print(findByText.evaluate());
+
+    expect(findByText.evaluate().isEmpty, false);
+  });
+
   //test_if_the_image_asset_loads_in_the_page
   //test_if_text_is_on_the_page
   //test_if_the_pages are_swapping_to_the_left
