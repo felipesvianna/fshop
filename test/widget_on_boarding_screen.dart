038b3543602248a7642ce83c9126b43f719aa3ff
Felipe <felipe.vianna10@gmail.com>
2019-03-10 19:28:56 -0300

    test: Defines tests to do on on boarding screen

--- test/widget_on_boarding_screen.dart
+++ test/widget_on_boarding_screen.dart
@@ -3,10 +3,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fshop/screens/onboarding_screen/onboarding_screen.dart';
 import './testing_util.dart';
+import 'package:fshop/screens/onboarding_screen/onboarding_screen.dart';
 
 void main() {
-  
+  //test_if_onboarding_screen_loads
+  //test_if_the_image_asset_loads_in_the_page
+  //test_if_text_is_on_the_page
+  //test_if_the_pages are_swapping_to_the_left
+  //test_if_the_skip_button_is_working
 }
 
