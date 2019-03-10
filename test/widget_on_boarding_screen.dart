85c5e363df01f201dce470de6127c60ae499e593
Felipe <felipe.vianna10@gmail.com>
2019-03-10 19:22:28 -0300

    test: Create widget_on_boarding_screen test file

---
+++ test/widget_on_boarding_screen.dart
@@ -0,0 +1,12 @@
+// This is the onboarding_screen widget test file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:fshop/screens/onboarding_screen/onboarding_screen.dart';
+import './testing_util.dart';
+
+void main() {
+  
+}
+
