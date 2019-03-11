681a2e1f14924f22c91d1bb41e3bb3de05daa6a7
Felipe <felipe.vianna10@gmail.com>
2019-03-10 22:00:26 -0300

    refactor: Remove the 'new' keywords from the new classes instances

--- test/testing_util.dart
+++ test/testing_util.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 
 Widget buildTestableWidget(Widget widget) {
   // https://docs.flutter.io/flutter/widgets/MediaQuery-class.html
-  return new MediaQuery(
-    data: new MediaQueryData(),
-    child: new MaterialApp(home: widget)
+  return MediaQuery(
+    data: MediaQueryData(),
+    child: MaterialApp(home: widget)
   );
 }
