cebcf637a3a8430edb6bb87a16b68b32e5711836
Felipe <felipe.vianna10@gmail.com>
2019-03-11 11:50:43 -0300

    test: Implement test_if_the_pages_has_content_and_are_swapping_to_the_left and refactor the other tests

--- test/widget_on_boarding_screen_test.dart
+++ test/widget_on_boarding_screen_test.dart
@@ -6,43 +6,78 @@ import 'package:flutter_test/flutter_test.dart';
 import './testing_util.dart';
 import 'package:fshop/screens/onboarding_screen/onboarding_screen.dart';
 
+void _showDebugDetailsPrintTextEvaluate (bool details) {
+  var findByText;
+
+  if(details){
+    findByText = find.byType(Text);
+    print(findByText.evaluate());
+  }
+}
+
 void main() {
   testWidgets('test_if_onboarding_screen_loads', (WidgetTester tester) async {
+    bool debugDetails = false; //Enable prints for debugging
+
     await tester.pumpWidget(buildTestableWidget(OnBoardingPage()));
     await tester.pumpAndSettle();
+    
+    expect(find.byType(Text).evaluate().isEmpty, false);
 
-    var findByText = find.byType(Text);
-    print(findByText.evaluate());
-
-    expect(findByText.evaluate().isEmpty, false);
+    _showDebugDetailsPrintTextEvaluate(debugDetails);
   });
 
   testWidgets('test_if_the_skip_button_is_working', (WidgetTester tester) async {
     print('test_if_the_skip_button_is_working');
 
     Finder skipButton;
-    //var findByText;
+    bool debugDetails = false; //Enable prints for debugging
 
     await tester.pumpWidget(buildTestableWidget(OnBoardingPage()));
     skipButton = find.byKey(Key('skipButton'));
 
     if(skipButton.evaluate().isNotEmpty){ //Check if can find the tappable text
-      //findByText = find.byType(Text);
-      //print(findByText.evaluate());
       expect(find.text('Oi'), findsOneWidget);
+
+      _showDebugDetailsPrintTextEvaluate(debugDetails);
       
       //By now, its routing to NotFoundPage. Later, change to HomePage screen
       await tester.tap(skipButton);
       await tester.pumpAndSettle();
-      //findByText = find.byType(Text);
-      //print(findByText.evaluate());
       expect(find.text('Em desenvolvimento'), findsOneWidget);
+
+      _showDebugDetailsPrintTextEvaluate(debugDetails);
     }
   });
 
-  //test_if_theres_text_is_on_the_page
-  //test_if_the_pages are_swapping_to_the_left
-  
+  testWidgets('test_if_the_pages_has_content_and_are_swapping_to_the_left', (WidgetTester tester) async {
+    bool debugDetails = false; //Enable prints for debugging
+    
+    await tester.pumpWidget(buildTestableWidget(OnBoardingPage()));
+    
+    _showDebugDetailsPrintTextEvaluate(debugDetails);
+    
+    //Test page 1
+    expect(find.text('Oi'), findsOneWidget);
+
+    //Swap to page 2
+    await tester.drag(find.byType(Image), const Offset(-500.0, 0.0));
+    await tester.pumpAndSettle();
+
+    //Test page 2
+    expect(find.text('Encontre seu produto'), findsOneWidget);
+    
+    _showDebugDetailsPrintTextEvaluate(debugDetails);
+
+    //Swap to page 3
+    await tester.drag(find.byType(Image), const Offset(-500.0, 0.0));
+    await tester.pumpAndSettle();
+
+    //Test page 3
+    expect(find.text('Efetue a compra'), findsOneWidget);
+
+    _showDebugDetailsPrintTextEvaluate(debugDetails);
+  }); 
 
   //test_if_the_image_asset_loads_in_the_page
 }
