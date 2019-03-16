4919a565ca2887b35e9ac7ba6d445b27f68ea110
Felipe <felipe.vianna10@gmail.com>
2019-03-16 11:15:54 -0300

    refactor: Move the methods from these files to login_page.dart

--- lib/screens/login_screen/login_page.dart
+++ lib/screens/login_screen/login_page.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
 
 import './generate_login_form.dart';
-import './generate_social_media_buttons.dart';
-import './generate_signup_link.dart';
+import '../../util/tapableText.dart';
 
 class LoginPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _LoginPageState();
   }
+}
+
+Widget _buildSignUpLink(){
+  return Container(
+          padding: EdgeInsets.only(top: 20, bottom: 14),
+          alignment: Alignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Novo por aqui? ', style: TextStyle(fontSize: 14),),
+                tapableText(Text('Cadastre-se!', key: Key('signup_tappable_text'), style: TextStyle(fontSize: 14, color: Colors.red, decoration: TextDecoration.underline)),
+                (){print('Cadastre-se tocado');}
+              )
+            ]
+          )
+        );
+}
+
+Widget _buildSocialMediaButtons(){
+  return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            RaisedButton(
+              key: Key('facebook_raised_button'),
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text("Facebook"),
+              onPressed: () {},
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+            ),
+            RaisedButton(
+              key: Key('google_raised_button'),
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              textColor: Colors.white,
+              color: Colors.red,
+              child: Text("Google"),
+              onPressed: () {},
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+            )
+          ]
+        );
 }
 
 class _LoginPageState extends State<LoginPage>{
@@ -31,8 +72,8 @@ class _LoginPageState extends State<LoginPage>{
                   image: AssetImage("assets/img/logo.png")
                 ),
                 generateLoginForm(context), //It returns a form
-                generateSocialMediaButtons(), //It returns a row
-                generateSignUpLink(), //It returns a container
+                _buildSocialMediaButtons(), //It returns a row
+                _buildSignUpLink(), //It returns a container
               ],
             ),
           ),
