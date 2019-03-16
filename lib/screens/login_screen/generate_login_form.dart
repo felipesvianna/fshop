2a86c917940829a0ef7699c59d635ecd48ba17db
Felipe <felipe.vianna10@gmail.com>
2019-03-16 11:36:24 -0300

    refactor: Make build methods for Form elements

--- lib/screens/login_screen/generate_login_form.dart
+++ lib/screens/login_screen/generate_login_form.dart
@@ -1,6 +1,53 @@
 import 'package:flutter/material.dart';
 
 import '../../util/tapableText.dart' show tapableText;
+import '../../screens/onboarding_screen/onboarding_screen.dart' show OnBoardingPage;
+
+
+Widget _buildEmailField() {
+  return TextFormField(
+            key: Key('email_form_field'),
+            decoration: InputDecoration(
+              labelText: "E-Mail"
+            ),
+            keyboardType: TextInputType.emailAddress,
+          );
+}
+
+Widget _buildPasswordField() {
+  return TextFormField(
+            key: Key('password_form_field'),
+            decoration: InputDecoration(
+              labelText: "Senha"
+            ),
+            keyboardType: TextInputType.text,
+            obscureText: true,
+          );
+}
+Widget _buildForgotPasswordButton() {
+  return Container(
+            padding: EdgeInsets.only(top: 20, bottom: 10),
+            alignment: Alignment.centerRight,
+            child: tapableText(Text('Esqueceu a senha?', key: Key('forgot_password_tappable_text'), style: TextStyle(fontSize: 12)),
+              (){print('Esqueceu a senha? tocado');})
+          );
+}
+Widget _buildLoginButton(context) {
+  return RaisedButton(
+            key: Key('login_raised_button'),
+            padding: EdgeInsets.symmetric(horizontal: 80.0),
+            textColor: Colors.white,
+            color: Colors.green,
+            child: Text("LOGIN"),
+            onPressed: () {
+              //Navigator.pushNamed(context, '/onBoardingRoute');
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (BuildContext context) => OnBoardingPage())
+              );//For testing. Can't test with pushReplacementNamed
+            },
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+        );
+}
 
 Widget generateLoginForm (BuildContext context){
   return Form(
@@ -9,38 +56,10 @@ Widget generateLoginForm (BuildContext context){
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  TextFormField(
-                    key: Key('email_form_field'),
-                    decoration: InputDecoration(
-                      labelText: "E-Mail"
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextFormField(
-                    key: Key('password_form_field'),
-                    decoration: InputDecoration(
-                      labelText: "Senha"
-                    ),
-                    keyboardType: TextInputType.text,
-                    obscureText: true,
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 20, bottom: 10),
-                    alignment: Alignment.centerRight,
-                    child: tapableText(Text('Esqueceu a senha?', key: Key('forgot_password_tappable_text'), style: TextStyle(fontSize: 12)),
-                      (){print('Esqueceu a senha? tocado');})
-                  ),
-                  RaisedButton(
-                    key: Key('login_raised_button'),
-                    padding: EdgeInsets.symmetric(horizontal: 80.0),
-                    textColor: Colors.white,
-                    color: Colors.green,
-                    child: Text("LOGIN"),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/onBoardingRoute');
-                    },
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-                  ),
+                  _buildEmailField(),
+                  _buildPasswordField(),
+                  _buildForgotPasswordButton(),
+                  _buildLoginButton(context)
                 ]
               )
     ),
